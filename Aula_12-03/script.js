ae885e353980ce4a10236e0d7ad760989a58077d
KevenRgFS <kevenregioFS@gmail.com>
2026-03-12 12:47:22 -0300

    12-03

--- Aula_12-03/script.js
+++ Aula_12-03/script.js
@@ -23,3 +23,35 @@ let destino = document.getElementById("elemento"); //buscando o elemento
 let p = document.createElement("p"); //cria o elemento HTML
 p.textContent= "Paragrafo criado via Javascript";
 destino.append(p); //adiciona ao DOM
+
+//criando a lista
+
+let Compras = document.getElementById("lista");
+let ul = document.createElement("ul");
+let itens = ["arroz", "queijo", "iogurt"];
+for(let i = 0; i<itens.length; i++){
+    let li = document.createElement("li");
+    li.textContent = itens[i];
+    ul.append(li);
+}
+lista.append(ul);
+
+//função somar
+
+function somar(){
+    //pegar os dois valores digitados pelo usuário
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let soma = n1+n2;
+    //console.log(soma);
+    let saida = `Seu resultado é: <b>${soma}</b>`;
+    document.getElementById("resultado").innerHTML = saida;
+}
+
+//eventos = botão
+let botao = document.getElementById("botao1");
+botao.onclick = function(){
+    alert("Clicou!");
+    botao.textContent = "Você clicou!";
+    botao.style.backgroundColor = "red";
+}
