f75abf4870f142990a765593ffe3d11d7e2d48cd
KevenRgFS <kevenregiofs@gmail.com>
2026-04-04 15:32:57 -0300

    ATV4

---
+++ Atividade4/script.js
@@ -0,0 +1,64 @@
+//criar os preços do combustível
+const precoGasolina = 6.69;
+const precoEtanol = 4.30;
+const precoDiesel = 6.03;
+
+//------------------------------------------------------------------------------------------------------------------------
+
+const atualizarValor = () =>{
+    let tipo = document.getElementById("combustivel").value;
+    console.log(tipo);
+
+        const precos = {
+            "gasolina": precoGasolina,
+            "etanol": precoEtanol,
+            "diesel": precoDiesel
+        };
+
+    let precoPorLitro = precos[tipo];
+
+        if(!precoPorLitro){
+            document.getElementById("resultado").textContent = "Selecione uma opção válida";
+            return;
+        }
+    console.log(precoPorLitro);
+
+    let litros = parseFloat(document.getElementById("litros").value);
+    calcularValorAbastecimento(precoPorLitro, litros);
+};
+
+let tipoCombustivel = document.getElementById("combustivel");
+tipoCombustivel.addEventListener("change", atualizarValor);
+
+//------------------------------------------------------------------------------------------------------------------------
+
+const calcularValorAbastecimento = (precoCombustivel, litros) => {
+
+        if(litros<=0 || isNaN(litros)){
+            document.getElementById("resultado").textContent = "Insira valor válido";
+            return;
+        }
+
+    let valorTotal = precoCombustivel * litros;
+    document.getElementById("resultado").textContent = formatarMoeda(valorTotal);
+};
+
+//------------------------------------------------------------------------------------------------------------------------
+
+const formatarMoeda = (valor) => {
+    return "R$ " + valor.toLocaleString("pt-br", 
+        {minimumFractionDigits: 2, maximumFractionDigits:2}
+    );
+};
+
+let litros = document.getElementById("litros");
+litros.addEventListener("input", atualizarValor);
+
+//------------------------------------------------------------------------------------------------------------------------
+
+litros.addEventListener ("keydown", (event) => {
+    if(event.key == "Enter"){
+        event.preventDefault();
+        atualizarValor();
+    };
+});
